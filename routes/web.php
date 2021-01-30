9507b4af600e3cd49b068fdc1d4ba76431211a5c
youcefkh <youcefkh13.yk@gmail.com>
2021-01-30 21:48:17 +0000

    add login route

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('master');
+});
+Route::get('/login', function () {
     return view('auth/login');
 });
 
 Auth::routes();
+
+
+
 
